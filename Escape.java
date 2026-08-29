ac31e4c3189ec808bbebfb2091fab4f938182b70
Christian James Urbano <cj.urbano2008@gmail.com>
2026-08-29 10:07:01 +0800

    Escape Act. 2.15

---
+++ Escape.java
@@ -0,0 +1,8 @@
+public class Escape
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("First Name\tLast Name\tStudent Number\tPhone Number");
+        System.out.println("Christian James Urbano\t\t26-189\t\t09457295281");
+    }
+}
