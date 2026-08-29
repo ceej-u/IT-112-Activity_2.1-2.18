6280b90536de61464cd9f0e579463e73d52b2370
Christian James Urbano <cj.urbano2008@gmail.com>
2026-08-29 10:17:10 +0800

    Box Act. 2.17

---
+++ Box.java
@@ -0,0 +1,22 @@
+//Urbano Christian James/BSIT-NETSEC/1st Year/1-1
+public class Box
+{
+    public static void main(String[] args) 
+{
+        System.out.println("*********");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*********");
+
+        System.out.println("   *");
+        System.out.println("  *  *");
+        System.out.println(" *    *");
+        System.out.println("*      *");
+        System.out.println(" *    *");
+        System.out.println("  *  *");
+        System.out.println("   *");
+    }
+}
