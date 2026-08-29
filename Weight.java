3203f051f38770d45818fea48bf5e2332ec26a46
Christian James Urbano <cj.urbano2008@gmail.com>
2026-08-29 09:34:53 +0800

    Weight Act. 2.8

---
+++ Weight.java
@@ -0,0 +1,12 @@
+//Urbano Christian James/BSIT-NETSEC/1st Year/1-1
+public class Weight 
+{
+    public static void main(String[] args) 
+    {
+        double pounds = 10;
+        double kilograms;
+
+        kilograms = pounds * 0.454;
+        System.out.println(pounds + " pounds is " + kilograms + " kilograms.");
+    }
+}
