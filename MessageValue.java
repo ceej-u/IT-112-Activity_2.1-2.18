7de52f8325ea57be3431eb479c5273d688bffb46
Christian James Urbano <cj.urbano2008@gmail.com>
2026-08-29 09:21:34 +0800

    MessageValue Act. 2.6
    MessageValue Act. 2.6

---
+++ MessageValue.java
@@ -0,0 +1,12 @@
+//Urbano Christian James/BSIT-NETSEC/1st Year/1-1
+public class MessageValue 
+{
+    public static void main(String[] args) 
+    {
+        String message = "The value of pie is:";
+        float value = 3.1415f;
+
+        System.out.println(message);
+        System.out.println(value);
+    }
+}
